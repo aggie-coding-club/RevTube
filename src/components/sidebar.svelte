1cf495de8d94410ca01df9587f8117afcd87d935
WarrenWu4 <warrenweiwu04@gmail.com>
2023-03-08 17:59:38 -0600

    added svelte icons

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,11 +1,14 @@
 <script>
-	import './styles.css';
+	import '../routes/styles.css';
     import { SvelteComponentTyped } from 'svelte';
-    import { Box, Button } from 'chakra-ui-svelte';
+	import FaAndroid from 'svelte-icons/fa/FaAndroid.svelte';
 </script>
 
 <div class="sidebar">
-	<div class="sidebar-container">
+	<div class="icon">
+		<FaAndroid></FaAndroid>
+	</div>
+	<div class="sidebar-btn-container">
 	</div>
 </div>
 
@@ -14,5 +17,9 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh; width: 10rem;
+	}
+	.icon {
+		font-size: 10rem;
+		color: black;
 	}
 </style>
