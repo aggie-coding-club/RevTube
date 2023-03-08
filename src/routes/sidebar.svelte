84d46fa60eb375a61715ee13e201d71729a7a99b
WarrenWu4 <warrenweiwu04@gmail.com>
2023-03-08 17:10:33 -0600

    skeleton sidebar

---
+++ src/routes/sidebar.svelte
@@ -0,0 +1,19 @@
+<script>
+	import './styles.css';
+    import { SvelteComponentTyped } from 'svelte';
+    import { Box, Button } from 'chakra-ui-svelte';
+</script>
+
+<div class="sidebar">
+	<div class="sidebar-container">
+		<div></div>
+	</div>
+</div>
+
+<style>
+	.sidebar {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh; width: 10rem;
+	}
+</style>
