8c718d611c4cedcf6c220314ba374f3049927e85
Redninjale <redninjale@gmail.com>
2023-03-29 17:53:59 -0500

    Removed boiler plate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,8 @@
 <div class="app">
 	<main>
 		<slot />
+		<Sidebar />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,6 @@
 </svelte:head>
 
 <section>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
 </section>
 
 <style>
