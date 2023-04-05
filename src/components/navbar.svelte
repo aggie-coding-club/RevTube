e273d8fb198ec50e4dac52b1b984ef727570ba59
augyr19 <august.ryan@tamu.edu>
2023-04-05 17:32:58 -0500

    Added search bar!

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div class="navbar">
+    <input type="text" placeholder="Search..">
 </div>
+
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
@@ -12,4 +14,15 @@
         background-color: #1a1717;
         position: sticky; top:0; left:0;
     }
+    /* Style the search box inside the navigation bar */
+    .navbar input[type=text] {
+    float: right;
+    padding: 6px;
+    border: none; 
+    margin-top: 12px;
+    margin-right: calc(40vw);
+    width: calc(20vw);
+    font-size: 17px;
+    }
 </style>
+
